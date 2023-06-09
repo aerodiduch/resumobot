bb84bac465859d929bcc2fdb5dad83e027ea16ed
Federico Perez Diduch <fperezdiduch@hotmail.com>
2023-06-08 23:38:28 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,109 @@
+import openai
+from discord import Intents
+import discord
+import random
+
+# Discord Token
+
+TOKEN = ""
+intents = Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+
+
+# OpenAI api key
+
+openai.api_key = ""
+chatbot = openai.Completion()
+
+
+funny_answers = [
+    "🤓 Dame un minutito que me pongo los lentes... ",
+    "👀 Leyendo...",
+    "✍️ Como no! Resumiendo... ",
+    "👾 Dame un toquesito...",
+    "🤖 Beep boop! A la orden!",
+]
+
+
+@client.event
+async def on_ready():
+    """Everytime bot is up and working, this will be printed to stdout"""
+    print(f"{client.user} has connected to Discord!")
+
+
+@client.event
+async def on_guild_join(guild):
+    channel = guild.text_channels[0]
+    await channel.send(
+        "👋 Hola! Soy ResumoBot, un bot creado para ayudarte a resumir las conversaciones en canales de texto. ¿Mucho texto? ¿Entraste a un canal y no queres leer todo? ¡Etiquetame y mirá la magia!"
+    )
+
+
+@client.event
+async def on_message(message):
+    """Action to be performed every time a message is send to a channel in a discord server.
+    In this particular case, everytime the bot is mentioned, it'll get the latest 50 messages in desc order,
+    and send to OpenAI to be summarized.
+
+    Args:
+        message (message): Message object from discord.
+    """
+    if message.author == client.user:
+        return
+
+    if client.user.mentioned_in(message):
+        # If and only if the bot is mentioned.
+        message_id = message.id
+        channel_id = message.channel.id
+        channel = client.get_channel(channel_id)
+
+        await message.add_reaction("✈️")
+        await channel.send(random.choice(funny_answers))
+
+        channel_history = await get_channel_history(channel)
+        ai_response = await get_summary_from_history(chatbot, channel_history)
+
+        await channel.send(ai_response)
+
+
+async def get_channel_history(channel) -> str:
+    """Receives a discord.Channel object, extracting it's message history up to the latest 50 messages.
+
+    Args:
+        channel (discord.Channel): Channel object from Discord.
+
+    Returns:
+        str: A concatenated string containg the last 50 messages on a specified channel.
+    """
+    messages = []
+    async for message in channel.history(limit=50, oldest_first=False):
+        messages.append(message.content)
+
+    # This way we get desc order, older messages last.
+    messages.reverse()
+    return "\n".join(messages)
+
+
+async def get_summary_from_history(chatbot, history: str) -> str:
+    """Based on a chatbot object using OpenAI token and channel history as string, this generates a summary prompt
+    using OpenAI's text-davinci-003 model by default.
+
+    Args:
+        chatbot (_type_): OpenAI object initialized with api key.
+        history (str): channel's message history in a string format.
+
+    Returns:
+        _type_: _description_
+    """
+    response = chatbot.create(
+        engine="text-davinci-003",
+        prompt=f"Eres un bot de Discord que se encarga de leer los ultimos mensajes de un canal y dar un resumen. Solo responde con el resumen: {history}",
+        max_tokens=500,
+    )
+
+    return str(response.choices[0].text)
+
+
+if __name__ == "__main__":
+    client.run(TOKEN)
---
+++ readme.md
@@ -0,0 +1,51 @@
+
+# ResumoBot
+
+Un pequeño bot que conecta Discord con OpenAI pensado para generar un resumen de las conversaciones que ocurren en los canales de un servidor.
+
+# Prerequisitos
+Actualmente este bot no está hosteado en ninguna plataforma, por lo cual para 
+agregarlo a cualquier servidor, debe ser buildeado en origen y cumplir con las siguientes condiciones:
+
+- API KEY de OpenAI (servicio de pago) **[DOCUMENTACION PENDIENTE]**
+- API KEY de Discord (servicio gratuito) **[DOCUMENTACION PENDIENTE]**
+
+# Instalación
+
+Una obtenida ambas API KEYs, podemos ejecutar iniciarlizar el bot de forma local.
+Este proyecto utiliza **entorno virtual de venv** para aislar el bot de las demas dependencias del sistema. 
+
+## Linux
+```bash
+git clone https://github.com/aerodiduch/ResumoBot.git && cd $_
+python3 -m venv venv
+source venv/bin/activate && pip install -r requirements.txt
+```
+
+Luego, ejecutamos nuestro editor de texto favorito para popular los campos `TOKEN` y `openai.api_key`. Hecho esto, ejecutamos el bot.
+
+```
+python3 main.py
+```
+
+Output:
+
+```sh
+❯ python main.py
+2023-06-08 23:15:24 INFO     discord.client logging in using static token
+2023-06-08 23:15:26 INFO     discord.gateway Shard ID None has connected to Gateway (Session ID: XXXX).
+MiBotCool#XXXX has connected to Discord!
+```
+
+# Utilizarlo
+
+Para poder darle uso a este bot, simplemente tenemos que etiquetarlo en el canal que deseamos generar un resumen. Luego de unos segundos cuando sea satisfcatoria la llamada a OpenAI, obtendremos el resumen.
+
+
+# WIP
+Este proyecto es un Work In Progress, por lo cual estará sujeto a modificaciones constantes con el objetivo de mejorar la experiencia del usuario
+
+Futuro:
+- Dockerizacion del proyecto
+- Documentacion de generación de llaves de Discord (tutorial de bot)
+- Documentación de generación de llaves de OpenAI
